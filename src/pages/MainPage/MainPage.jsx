602f4b968f5479aab0ae0e6ba7efe7f57270692f
Slavik-Da <Slav140k@ukr.net>
2021-12-12 15:07:12 +0200

    feat: Main Page in Emotion.js

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import * as S from './styled'
+
+export const MainPage = () => {
+  return (
+    <>
+      <S.MainPage>
+        Main page
+      </S.MainPage>
+    </>
+  )
+}
---
+++ src/pages/MainPage/styled/index.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const MainPage = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: beige;
+  display: flex;
+`
